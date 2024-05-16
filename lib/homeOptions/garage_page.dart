369358feb2b2246e15e50a8631e53ba0bf1fc2ea
Matei <matei.ciocanescu10@gmail.com>
2024-05-16 16:37:26 +0300

    Added warning banner in Garage page for guest users to create accounts.

--- lib/homeOptions/garage_page.dart
+++ lib/homeOptions/garage_page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:automate/otherWidgets/create_account_banner.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +20,7 @@ class GaragePage extends StatefulWidget {
 class _GaragePageState extends State<GaragePage> {
   List<Vehicle> _vehicles = [];
   bool _isLoading = true;
+  late bool _isWarningVisible;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   @override
@@ -31,7 +33,6 @@ class _GaragePageState extends State<GaragePage> {
     setState(() {
       _isLoading = true;
     });
-
     if (widget.user != null) {
       var collection = _firestore.collection('users').doc(widget.user!.uid).collection('vehicles');
       var snapshot = await collection.get();
@@ -39,6 +40,7 @@ class _GaragePageState extends State<GaragePage> {
       setState(() {
         _vehicles = vehicles;
         _isLoading = false;
+        _isWarningVisible = false;
       });
     } else {
       final prefs = await SharedPreferences.getInstance();
@@ -51,6 +53,7 @@ class _GaragePageState extends State<GaragePage> {
         });
       }
       setState(() {
+        _isWarningVisible = true;
         _isLoading = false;
       });
     }
@@ -119,71 +122,79 @@ Widget build(BuildContext context) {
     return Scaffold(
         body: _isLoading
             ? const Center(child: CircularProgressIndicator()) // Show loading indicator while fetching data
-            : _vehicles.isEmpty
-                ? const Center(child: Text('Add your first vehicle')) // Show this text if no vehicles are present
-                : ListView.builder( // Show this list if there are vehicles
-                    itemCount: _vehicles.length,
-                    itemBuilder: (context, index) {
-                        final vehicle = _vehicles[index];
-                        return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                            child: GestureDetector(
-                                onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => EditVehiclePage(
-                                                name: vehicle.name,
-                                                vinNumber: vehicle.vinNumber,
-                                                editVehicleCallback: (name, vin) {
-                                                    _editVehicle(index, name, vin);
-                                                },
-                                            ),
-                                        ),
-                                    );
-                                },
-                                child: Stack(
-                                    children: [
-                                        Container(
-                                            height: 60,
-                                            decoration: BoxDecoration(
-                                                color: Colors.grey[300],
-                                                borderRadius: BorderRadius.circular(20.0),
-                                            ),
-                                            child: Row(
-                                                children: [
-                                                    const SizedBox(width: 20),
-                                                    Text(
-                                                        vehicle.name,
-                                                        style: const TextStyle(fontSize: 18.0),
-                                                    ),
-                                                    const Spacer(),
-                                                    GestureDetector(
-                                                        onTap: () => _removeVehicle(index),
-                                                        child: Container(
-                                                            width: 40,
-                                                            alignment: Alignment.center,
-                                                            decoration: const BoxDecoration(
-                                                                shape: BoxShape.circle,
-                                                                color: Colors.red,
-                                                            ),
-                                                            child: const Icon(
-                                                                Icons.close,
-                                                                color: Colors.white,
-                                                                size: 24,
-                                                            ),
-                                                        ),
-                                                    ),
-                                                    const SizedBox(width: 20),
-                                                ],
-                                            ),
-                                        ),
-                                    ],
-                                ),
-                            ),
-                        );
-                    },
-                ),
+            : Column(
+              children: <Widget>[
+                widget.user == null &&  _isWarningVisible ? const AccountWarningBanner() : Container(),
+                Expanded(
+                  child:
+                  _vehicles.isEmpty
+                      ? const Center(child: Text('Add your first vehicle')) // Show this text if no vehicles are present
+                      : ListView.builder( // Show this list if there are vehicles
+                          itemCount: _vehicles.length,
+                          itemBuilder: (context, index) {
+                              final vehicle = _vehicles[index];
+                              return Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                                  child: GestureDetector(
+                                      onTap: () {
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) => EditVehiclePage(
+                                                      name: vehicle.name,
+                                                      vinNumber: vehicle.vinNumber,
+                                                      editVehicleCallback: (name, vin) {
+                                                          _editVehicle(index, name, vin);
+                                                      },
+                                                  ),
+                                              ),
+                                          );
+                                      },
+                                      child: Stack(
+                                          children: [
+                                              Container(
+                                                  height: 60,
+                                                  decoration: BoxDecoration(
+                                                      color: Colors.grey[300],
+                                                      borderRadius: BorderRadius.circular(20.0),
+                                                  ),
+                                                  child: Row(
+                                                      children: [
+                                                          const SizedBox(width: 20),
+                                                          Text(
+                                                              vehicle.name,
+                                                              style: const TextStyle(fontSize: 18.0),
+                                                          ),
+                                                          const Spacer(),
+                                                          GestureDetector(
+                                                              onTap: () => _removeVehicle(index),
+                                                              child: Container(
+                                                                  width: 40,
+                                                                  alignment: Alignment.center,
+                                                                  decoration: const BoxDecoration(
+                                                                      shape: BoxShape.circle,
+                                                                      color: Colors.red,
+                                                                  ),
+                                                                  child: const Icon(
+                                                                      Icons.close,
+                                                                      color: Colors.white,
+                                                                      size: 24,
+                                                                  ),
+                                                              ),
+                                                          ),
+                                                          const SizedBox(width: 20),
+                                                      ],
+                                                  ),
+                                              ),
+                                          ],
+                                      ),
+                                  ),
+                              );
+                          },
+                      ),
+                    ),
+              ],
+            ),
         floatingActionButton: FloatingActionButton(
             onPressed: () {
                 Navigator.push(
@@ -197,5 +208,4 @@ Widget build(BuildContext context) {
         ),
     );
 }
-
 }
---
+++ lib/otherWidgets/create_account_banner.dart
@@ -0,0 +1,73 @@
+import 'package:automate/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:automate/register.dart';
+
+class AccountWarningBanner extends StatefulWidget {
+  final Function(bool)? onDismiss; // Callback for handling visibility state externally if needed
+
+  const AccountWarningBanner({Key? key, this.onDismiss}) : super(key: key);
+
+  @override
+  _AccountWarningBannerState createState() => _AccountWarningBannerState();
+}
+
+class _AccountWarningBannerState extends State<AccountWarningBanner> {
+  bool _isVisible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_isVisible) return const SizedBox.shrink(); // Do not render anything if not visible
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.pink[100],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch, // Stretch the column to take full width
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Expanded(
+                child: Text(
+                  "We recommend creating an account to keep all your data saved.",
+                  style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.close, color: Colors.red),
+                onPressed: () {
+                  setState(() {
+                    _isVisible = false;
+                  });
+                  if (widget.onDismiss != null) widget.onDismiss!(_isVisible);
+                },
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0), // Add padding above the button for spacing
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RegisterPage()),
+                );
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(AppColors.snow),
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                maximumSize: MaterialStateProperty.all<Size>(const Size(double.infinity, 60)),
+              ),
+              child: const Text("Create Account"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
